07ddb139795a4fadab6054a85f8de21ee2616c80
krishhrajgithub <krishhrajgithub>
2025-07-11 09:57:56 +0000

    c

--- JPMC_JULY/DAY3/E2E_Kraj_ecom/transformations/gold.sql
+++ JPMC_JULY/DAY3/E2E_Kraj_ecom/transformations/gold.sql
@@ -15,3 +15,21 @@ join kraj_gold.customer_active c
 group by all
 order by total_sales desc
 limit 3;
+
+create materialized view kraj_gold.top_product_by_active_customer as
+select
+  c.customer_id,
+  c.customer_name,
+  p.product_id,
+  p.product_name,
+  p.product_category,
+  sum(s.total_amount) as total_sales,
+  count(distinct s.order_id) as order_count
+from kraj_gold.customer_active c
+join kraj_silver.sales_cleaned_pl s
+  on c.customer_id = s.customer_id
+join kraj_silver.products_cleaned p
+  on s.product_id = p.product_id
+group by all
+order by total_sales desc
+limit 10;
